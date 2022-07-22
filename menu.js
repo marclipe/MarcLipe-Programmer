7c3950af502655e204459782bf12c70685477e4f
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-07-21 23:41:04 -0300

    Common

--- menu.js
+++ menu.js
@@ -1,18 +1,11 @@
 let show = true;
 
-const menu = document.querySelector("")
-const menuToggle = menu.querySelector(".toggle")
 
-menuToggle.addEventListener("click", () => {
-    document.body.style.overflow = show ? "hidden" : "initial"
-    menu.classList.toggle("on", show);
-    show = !show;
-})
 
 /* HOME*/
 function fechar() {
     let inputCheckbox = document.getElementById('toggle');
-    document.body.style.overflow = show ? "hidden" : "initial"
+    //document.body.style.overflow = show ? "hidden" : "initial"
     if(inputCheckbox.checked === true){
         inputCheckbox.checked = false;
     }
