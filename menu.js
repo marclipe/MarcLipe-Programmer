db43bdc462b098d730e698668d4821692150223b
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-07-29 00:03:01 -0300

    Vamos lá

--- menu.js
+++ menu.js
@@ -1,15 +1,27 @@
-let show = true;
+
+//scroll
+let scroll = window.document.getElementById("scroll")
+let inputCheckbox = document.getElementById('toggle');
+
+scroll.addEventListener('click', clicar)
+function clicar() {
+    document.body.style.overflow = scroll ? "hidden" : "initial"
+    
+    scroll =! scroll    
+}
 
 
 
-/* HOME*/
+
+/* HOME
 function fechar() {
-    let inputCheckbox = document.getElementById('toggle');
+    
     //document.body.style.overflow = show ? "hidden" : "initial"
     if(inputCheckbox.checked === true){
         inputCheckbox.checked = false;
     }
-}
+}*/
+
 
 
 
